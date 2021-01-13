104b0920269c8f7f00090fe4b8e4fa50e205785d
Gizem Pesen <pesengizem999@gmail.com>
2021-01-13 23:25:06 +0300

    train test sütünları hakkında bilgiler

--- titanic.py
+++ titanic.py
@@ -16,3 +16,23 @@ train_dataset = pd.read_csv(r'C:\Users\pesen\Desktop\new\train.csv')     #(r'C:\
 test_dataset = pd.read_csv(r'C:\Users\pesen\Desktop\new\test.csv')
 
 #---------------------------------------------------------------------------------------
+
+#Sütunları kontrol ediyoruz.
+print(train_dataset.columns )
+
+#---------------------------------------------------------------------------------------
+
+#İki veri setindeki toplam yolcu sayısı
+print(train_dataset.shape[0] + test_dataset.shape[0])
+
+#---------------------------------------------------------------------------------------
+
+#Hayatta kalma oranı
+train_dataset['Survived'].mean()
+
+#---------------------------------------------------------------------------------------
+
+
+
+
+
